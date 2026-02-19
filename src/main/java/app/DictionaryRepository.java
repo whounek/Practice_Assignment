fe4edabdfbc7a63b5894cbc4a2acc8e2d393df2e
whounek <whounek8@gmail.com>
2026-02-19 21:39:15 +0300

    Задание 3 Spring, DB, Web

---
+++ src/main/java/app/DictionaryRepository.java
@@ -0,0 +1,8 @@
+package app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DictionaryRepository extends JpaRepository<DictionaryEntry, Long> {
+}
